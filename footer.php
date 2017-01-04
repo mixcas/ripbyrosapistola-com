858e5fa38ce16b4b9b99032c4c9ee596e5d85400
cas <cas@interglobal.vision>
2017-01-04 16:57:29 -0600

    add scaffold footer

--- footer.php
+++ footer.php
@@ -1,4 +1,25 @@
-  <footer id="footer">
+  <footer id="footer" class="container">
+    <div class="grid-row">
+      <div class="grid-item item-s-4">
+        <h4>Find us</h4>
+        <p>Lisboa #48, Colonia Juárez, México D.F.</p>
+
+        <p>Tel: +52 (55) 5264 8248</p>
+
+        <p>Hours<br/>
+        Mon- Fri 12:00 a 8:00<br/>
+        Sat 12:00 a 5:00 pm
+        </p>
+
+        <p>rip@ripbyrosapistola.com</p>
+      </div>
+      <div class="grid-item item-s-4">
+      </div>
+      <div class="grid-item item-s-4">
+        <h4>Follow us</h4>
+      </div>
+    </div>
+    <p class="text-align-center">R†P by Rosa Pistola · Copyright © 2017 · All Rights Reserved</p>
   </footer>
 
 </section>
