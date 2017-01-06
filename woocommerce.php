b19bb007bc0ad604bfb09f93f037131f2a1850cd
cas <cas@interglobal.vision>
2017-01-05 18:06:18 -0600

    add woommerce template

---
+++ woocommerce.php
@@ -0,0 +1,24 @@
+<?php
+get_header();
+?>
+
+<main id="main-content">
+  <section id="posts">
+    <div class="container">
+      <div class="grid-row">
+
+        <article class="grid-item item-s-12">
+          <?php woocommerce_content(); ?>
+        </article>
+
+      </div>
+    </div>
+  </section>
+
+  <?php get_template_part('partials/pagination'); ?>
+
+</main>
+
+<?php
+get_footer();
+?>
