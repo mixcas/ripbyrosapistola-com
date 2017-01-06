dcc983f67d519af29b41bcb48047d6c0b4936b55
cas <cas@interglobal.vision>
2017-01-06 14:00:06 -0600

    add background with imageset functions and image sizes

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -1,3 +1,18 @@
 <?php
 
 // Custom functions (like special queries, etc)
+function background_image_set_css($image_id = 0) {
+  $x1 = wp_get_attachment_image_src($image_id, 'background-image-1x');
+  $x1 = $x1[0];
+
+  $x2 = wp_get_attachment_image_src($image_id, 'background-image-2x');
+  $x2 = $x2[0];
+
+  $css = 'background-image: url(' . $x1 . '); ';
+  $css .= 'background-image: image-set( url(' . $x1 . ') 1x, url(' . $x2 . ') 2x); ';
+  $css .= 'background-image: -webkit-image-set( url(' . $x1 . ') 1x, url(' . $x2 . ') 2x); ';
+  $css .= 'background-image: -moz-image-set( url(' . $x1 . ') 1x, url(' . $x2 . ') 2x); ';
+  $css .= 'background-image: -o-image-set( url(' . $x1 . ') 1x, url(' . $x2 . ') 2x);';
+
+  return $css;
+}
--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -9,4 +9,7 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'opengraph', 1200, 630, true );
 
   add_image_size( 'gallery', 1200, 9999, false );
+
+  add_image_size( 'background-image-1x', 1200, 800, false );
+  add_image_size( 'background-image-2x', 2400, 1600, false );
 }
