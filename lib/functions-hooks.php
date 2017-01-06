8d68010fcee2f7b8c7b041ea7f1b58a5b66c6cbd
cas <cas@interglobal.vision>
2017-01-05 18:06:28 -0600

    add disable reviews

--- lib/functions-hooks.php
+++ lib/functions-hooks.php
@@ -1,3 +1,9 @@
 <?php
 
 // Custom hooks (like excerpt length etc)
+
+add_filter( 'woocommerce_product_tabs', 'wcs_woo_remove_reviews_tab', 98 );
+function wcs_woo_remove_reviews_tab($tabs) {
+  unset($tabs['reviews']);
+  return $tabs;
+}
