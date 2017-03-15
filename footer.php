1a600b716742b7fb4ad012e88ab097c35d923166
cas <cas@interglobal.vision>
2017-03-15 10:03:58 -0600

    fix footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
   <footer id="footer" class="bg-black white-text margin-top-basic padding-top-small padding-bottom-small">
     <div class="grid-row container margin-top-basic margin-bottom-basic">
-      <div class="grid-item item-s-4">
+      <div class="grid-item item-s-12 item-l-4">
         <h4 class="margin-bottom-tiny">Find us</h4>
         <p>Lisboa #48, Colonia Juárez, México D.F.</p>
 
@@ -13,17 +13,21 @@
 
         <p>rosapistola@gmail.com</p>
       </div>
-      <div id="footer-logo" class="grid-item item-s-4">
+      <div id="footer-logo" class="grid-item item-s-12 item-l-4">
         <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/rip_logo.svg'); ?>
       </div>
-      <div class="grid-item item-s-4">
+      <div class="grid-item item-s-12 item-l-4">
         <h4 class="margin-bottom-tiny">Follow us</h4>
-        <p><a href="https://www.facebook.com/Ripbyrosapistola" target="_blank" rel="noopener">facebook.com/Ripbyrosapistola</a></p>
-        <p><a href="https://www.instagram.com/rosapistola/" target="_blank" rel="noopener">instagram.com/rosapistola/</a></p>
-        <p><a href="https://twitter.com/rosapistola" target="_blank" rel="noopener">twitter.com/rosapistola</a></p>
+        <a class="social-icon" href="http://facebook.com/Ripbyrosapistola/" target="_blank" rel="nofollow"><?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/fb_logo.svg'); ?></a>
+        <a class="social-icon" href="https://www.instagram.com/rosapistola/" target="_blank" rel="nofollow"><?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/instagram_logo.svg'); ?></a>
+        <a class="social-icon" href="https://twitter.com/rosapistola" target="_blank" rel="noopener"><?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/twitter_logo.svg'); ?></a></p>
       </div>
     </div>
-    <p class="text-align-center">R†P by Rosa Pistola · Copyright © 2017 · All Rights Reserved</p>
+    <div class="grid-row">
+      <div class="grid-item item-s-12">
+        <p class="text-align-center">R†P by Rosa Pistola · Copyright © 2017 · All Rights Reserved</p>
+      </div>
+    </div>
   </footer>
 
 </section>
