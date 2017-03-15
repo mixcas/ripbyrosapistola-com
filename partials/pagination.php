1ceb8bc0230c0479e41d4700143e36049c619f58
cas <cas@interglobal.vision>
2017-03-15 10:43:25 -0600

    fix pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -2,22 +2,13 @@
 if( get_next_posts_link() || get_previous_posts_link() ) {
 ?>
   <!-- post pagination -->
-  <nav id="pagination" class="grid-row margin-top-basic margin-bottom-small container justify-between">
+  <nav id="pagination" class="grid-row margin-top-basic margin-bottom-small container justify-between font-bold">
 <?php
 $previous = get_previous_posts_link('< Newer');
 $next = get_next_posts_link('Older >');
-if ($previous) {
 ?>
     <div class="grid-item item-s-4"><?php echo $previous; ?></div>
-<?php
-}
-
-if ($next) {
-?>
     <div class="grid-item item-s-4 text-align-right"><?php echo $next; ?></div>
-<?php
-}
-?>
   </nav>
 <?php
 }
